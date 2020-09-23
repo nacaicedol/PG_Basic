760e726ec1c63112e01762b0da72a029b0f8ea97
nacaicedol <nacaicedol@unal.edu.co>
2020-09-23 13:13:41 -0500

    comit 1

--- mod-4/script.js
+++ mod-4/script.js
@@ -9,8 +9,8 @@
 // The program should say "Hello" to any name except names that start with a "J"
 // or "j", otherwise, the program should say "Good Bye". So, the final output
 // on the console should look like this:
-/*
-Hello Yaakov
+
+/*Hello Yaakov
 Good Bye John
 Good Bye Jen
 Good Bye Jason
